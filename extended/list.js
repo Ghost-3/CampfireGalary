7ddc2f6a2d523e8de6724f22adafa29a37e07164
Ghost <66458557+Ghost-3@users.noreply.github.com>
2022-07-18 03:07:45 +0300

    Scroll to top, input on enter, bug fix

--- extended/list.js
+++ extended/list.js
@@ -8,8 +8,20 @@ function onLoad() {
     Form = document.getElementById('Search');
     Form.addEventListener('submit', onClick);
     
-    input = document.getElementById("inputID");
-    input.placeholder = Number(id);
+    document.addEventListener("keyup", function(event) {
+        if (event.key == "ArrowLeft") {
+            document.getElementById('prev').click();
+        }
+        else if (event.key == "ArrowRight") {
+            document.getElementById('next').click();
+        }
+        else if (event.key == "Enter") {
+            document.getElementById('inputButton').click();
+        }
+    });
+    
+    input = document.getElementById("inputOffset");
+    input.placeholder = Number(offset);
     input.focus();
 }
 
@@ -83,6 +95,24 @@ function topNavSwitch() {
     } else {
         x.className = "topnav";
     }
+}
+
+function scrollToTop (duration) {
+    // cancel if already on top
+    if (document.scrollingElement.scrollTop === 0) return;
+    const cosParameter = document.scrollingElement.scrollTop / 2;
+    let scrollCount = 0, oldTimestamp = null;
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollCount will be Infinity
+            scrollCount += Math.PI * (newTimestamp - oldTimestamp) / duration;
+            if (scrollCount >= Math.PI) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = cosParameter + cosParameter * Math.cos(scrollCount);
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
 }
 
 onLoad();
